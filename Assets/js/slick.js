68114f5a6ca97f2b3ee23fae8603fe3396fec4f1
TeaNesx <nhatlong@hotmail.de>
2019-10-22 03:18:31 +0200

    (STYLE) Styling Silder for Landingpage

--- Assets/js/slick.js
+++ Assets/js/slick.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $(".single-item").slick();
+
+    //Move Button in DOM down
+    var prev = $(".slick-prev");
+    var next = $('.slick-next')
+    prev.next().insertBefore(prev);
+
+    prev.addClass('col-2 btn');
+    next.addClass('col-2 btn');
+
+
 });
